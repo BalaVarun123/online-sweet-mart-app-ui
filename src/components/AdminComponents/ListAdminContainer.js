8e09cbbd8ef45d42188a388bd28804e9e43355fa
BalaVarun123 <lsv.mail.id@gmail.com>
2021-04-29 20:48:19 +0530

    Added dummy files

---
+++ src/components/AdminComponents/ListAdminContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ListAdminComponent from "./ListAdminComponent";
+class ListAdminContainer extends React.Component{
+    render(){
+        return <div>
+            <ListAdminComponent/>
+        </div>
+    }
+}
+
+export default ListAdminContainer;
