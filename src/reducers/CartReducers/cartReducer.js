596f42caa79a04aff5d62ed7d550737446cd33ad
Ujjwal-Singh21 <you@example.com>
2021-05-05 14:51:11 +0530

    extra commments removed

--- src/reducers/CartReducers/cartReducer.js
+++ src/reducers/CartReducers/cartReducer.js
@@ -10,24 +10,24 @@ const cartReducer = (state = initialState, action) => {
     switch (action.type) {
         case CartActionTypes.ADD_CART:
             return { ...state, newcart: action.payload, isAdded: true }
-        //return [...state, action.cart]
+
 
         case CartActionTypes.UPDATE_CART:
             return { ...state, carts: action.payload, isUpdated: true }
-        //return [...state, action.cart]
+
 
         case CartActionTypes.CANCEL_CART:
             return { ...state, cart: action.carts }
-        //return[...state, action.cart]
+
 
         case CartActionTypes.SHOW_CART_BY_ID:
             return { ...state, cart: action.cart }
-        //return action.cart
+
 
         case CartActionTypes.SHOW_ALL_CARTS:
             console.log('reducer show all')
             return { ...state, carts: action.carts }
-        // return action.carts
+
 
         default:
             return state;
