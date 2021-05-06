7d545f03d56ce3fbe8a93d84b03581c00ffc33f3
BalaVarun123 <lsv.mail.id@gmail.com>
2021-05-06 19:52:38 +0530

    added dummy NavBar

---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { connect } from "react-redux";
+import { LoginService } from "../../services/LoginServices/LoginService";
+
+class NavBar extends React.Component{
+    render(){
+        return (<div>
+            <div class="ui menu">
+            <div class="header item">
+            Online Sweet Mart App
+             </div>
+              <a class="active item">
+                  Home
+                </a>
+                <a class="item">
+                 Messages
+                 </a>
+                <a class="item">
+                Friends
+                </a>
+                <div class="right menu">
+                <div class="item" onClick = {this.props.onClickLogOut()}>
+                    Logout
+                    </div>
+                    </div>
+                    </div>
+            </div>
+        );
+    }
+}
+const mapStateToProps = (state, props) => {
+return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+return {
+    onClickLogOut : () => {
+        // const loginService = new LoginService();
+        // loginService.logout(dispatch)
+    }
+}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(NavBar);
