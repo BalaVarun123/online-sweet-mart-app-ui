ded0903abde351f6f746662f010632c8c66eb915
Ujjwal-Singh21 <80033722+Ujjwal-Singh21@users.noreply.github.com>
2021-05-06 11:32:49 +0530

    Update AddCartComponent.js
    removed extra div tag

--- src/components/CartComponents/AddCartComponent.js
+++ src/components/CartComponents/AddCartComponent.js
@@ -25,7 +25,7 @@ class AddCartComponent extends React.Component {
 
             <div class = 'container' >
 
-            <div class = "container" >
+            
 
                 <p> {this.props.state.message} </p>
                 <form onSubmit={this.onSubmit} onReset={this.onReset} class="ui form" class="ui twelve wide column form segment">
