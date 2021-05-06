61291a201853c883eb4006d684b9d205cd29bf39
Ujjwal-Singh21 <80033722+Ujjwal-Singh21@users.noreply.github.com>
2021-05-06 17:56:14 +0530

    Update Footer.js

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -18,19 +18,7 @@ export class Footer extends Component {
 
                     </div>
 
-                </h4>                        
-
-                <h4 class="ui grey header">Got Question? | Contact us at
-                +917358068806 / +917358072807 <br/><br/> 
-                <div>    
-                    <i class="phone volume icon"></i> 
-                    <i class="comment alternate icon"></i>
-                    <i class="comments icon"> 
-                    </i> <i class="paper plane icon"></i>
-                </div>
-                </h4>         
-               
-
+                </h4>                       
 
             </div>
         )
